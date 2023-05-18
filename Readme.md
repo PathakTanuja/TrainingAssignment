c59e09ab08a0644981d959cfc6f10e932d7a20f2
TANUJA PATHAK <tanuja.pathak@turtlemint.com>
2023-05-18 16:02:54 +0530

    undefined vs null

--- Readme.md
+++ Readme.md
@@ -8,4 +8,13 @@ JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programm
 
 We have Number ,String, Boolean ,Undefined, And Null Primitive available in Java Script
 
+//Undefined Vs Null
+
+Undefined : only declare the variable  not assiging any value . var variable;
+console.log(variable);
+output undefined
+
+Null : declare the variable and assign the value ,but the value is null.
+var variable = null;
+
 
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-
+//Primitive Type
 var stringTypeVariable = "Hello";
 var numberTypeVariable = 23;
 var booleanTypeVariable = true;
+
 var undefinedTypeVariable ;
 var nullTypeVariable = null;
 
@@ -15,5 +16,23 @@ console.log("booleanTypeVariable " +booleanTypeVariable );
 console.log("undefinedTypeVariable " +undefinedTypeVariable );
 
 console.log("nullTypeVariable " +nullTypeVariable );
+
+
+//Undefine Vs Null
+
+// Declare the variable
+ var variable;
+ console.log(variable);   //undefined
+ variable =  10;
+ console.log(variable);  //10
+
+//Declare the varible and assign the null value
+
+var variable1 = null;
+console.log(variable1);    //null
+
+
+
+
 
 
