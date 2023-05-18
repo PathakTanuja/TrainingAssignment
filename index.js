e6d38a4563005ceeb389ece448405be45fd7e451
TANUJA PATHAK <tanuja.pathak@turtlemint.com>
2023-05-18 15:42:15 +0530

    Primitive type in JavaScript

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+var stringTypeVariable = "Hello";
+var numberTypeVariable = 23;
+var booleanTypeVariable = true;
+var undefinedTypeVariable ;
+var nullTypeVariable = null;
+
+console.log("stringTypeVariable " +stringTypeVariable );
+
+console.log("numberTypeVariable " +numberTypeVariable );
+
+console.log("booleanTypeVariable " +booleanTypeVariable );
+
+console.log("undefinedTypeVariable " +undefinedTypeVariable );
+
+console.log("nullTypeVariable " +nullTypeVariable );
+
+
