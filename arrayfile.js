8854b78379fb4d427faa91c7c6f80174d12d0072
TANUJA PATHAK <tanuja.pathak@turtlemint.com>
2023-05-19 16:39:27 +0530

    Array in Javascript

---
+++ arrayfile.js
@@ -0,0 +1,28 @@
+//create array 
+
+// arrayname = [] ;
+const employee = [];
+employee.push("Rahul");   // add value
+employee.push("Ajay");  
+employee.push("Sita");  
+employee.push("Radha");  
+console.log(employee);
+if(employee.length > 1) {   // arrayname.length  // length of array 
+    employee.pop();    // arrayname.pop()  // remove last element
+console.log(employee);
+ employee.shift();  // arrayname.shift()   // remove first element
+ console.log(employee);
+ employee.unshift("Mohan"); // arrayname.unshift()  //add new element in the first position
+ console.log(employee);
+}
+else {
+    console.log("no element in the array");
+    employee.unshift("Mohan"); // arrayname.unshift()  //add new element in the first position
+    console.log(employee);
+}
+
+
+
+
+
+
